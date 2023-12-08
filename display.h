62ec004e7df500ac5948926b15fbf668cdc0ae92
Kkeilaa <118190129+Kkeilaa@users.noreply.github.com>
2023-12-08 19:55:23 +0200

    Add files via upload

---
+++ display.h
@@ -0,0 +1,21 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+#include <arduino.h>
+
+extern int numerolista[10] = {1, 79, 18, 6, 76, 36, 32, 15, 0, 4};
+
+extern const int reset=6;
+extern const int shiftClock=5;
+extern const int latchClock=4;
+extern const int outEnable=3;
+extern const int serialInput=2;
+
+void initializeDisplay(void);
+
+void writeByte(uint8_t number, bool last);
+
+void writeHighAndLowNumber(uint8_t tens,uint8_t ones);
+
+void showResult(byte result);
+
+#endif
